501d4d489759f9bb6957e44622f8f0e8c4d3eb8f
迟国强 <chigq@aixiangdao.com>
2019-09-18 15:08:00 +0800

    用户协dfdf议ssdd

---
+++ GQViewController.h
@@ -0,0 +1,13 @@
+//
+//  GQViewController.h
+//  MYPodsTest
+//
+//  Created by 迟国强 on 09/18/2019.
+//  Copyright (c) 2019 迟国强. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GQViewController : UIViewController
+
+@end
---
+++ GQViewController.m
@@ -0,0 +1,30 @@
+//
+//  GQViewController.m
+//  MYPodsTest
+//
+//  Created by 迟国强 on 09/18/2019.
+//  Copyright (c) 2019 迟国强. All rights reserved.
+//
+
+#import "GQViewController.h"
+
+@interface GQViewController ()
+
+@end
+
+@implementation GQViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor orangeColor];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
